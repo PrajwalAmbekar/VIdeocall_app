ed6c8a27daa55c8e9e0efd545be7b36a6a91c37e
Prajwal <ambekarprajwal33@gmail.com>
2025-05-14 23:30:19 +0530

    login lolgout and stream_app authentication is setup

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,6 +1,8 @@
 import { User } from '../model/auth.model.js';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import bcrypt from 'bcryptjs';
+import { upsertServerClient } from '../lib/stream.js';
 dotenv.config();
 
 
@@ -37,16 +39,37 @@ export const signup = async (req, res) => {
       profilePic: ranndomAvatar,
       username
     });
+    await newUser.save();
+    try {
+      await upsertServerClient({
+        id: newUser._id.toString(),
+        name: newUser.username,
+        image: newUser.profilePic
+      });
+      console.log('User upserted successfully');
+      console.log(`Stream user created for user: ${newUser.username}`);
+
+    } catch (error) {
+      console.log('Error in upsertServerClient:', error);
+      return res.status(500).json({ message: 'Error creating user in Stream' });
+      
+    }
     const generatedToken = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
       expiresIn: '1h'
     });     
     console.log('Generated token:', generatedToken);
+
+    res.cookie('token', generatedToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // Set to true in production
+      maxAge: 3600000 // 1 hour
+    });
      
     newUser.token = generatedToken;
     console.log('User document before saving:', newUser);   
     
 
-    await newUser.save();
+   
     res.status(201).json({ message: 'User created successfully' });
 
   } catch (error) {
@@ -60,7 +83,32 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
 
     try {
-        res.send('Login endpoint');
+        const { email, password } = req.body;
+        console.log('Received data:', email, password);
+        if (!email || !password) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+        const user = await User.findOne({ email }).select('+password');
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+        const isPasswordMatch =await user.comparePassword(password);
+        console.log('Password match:', isPasswordMatch);
+        if (!isPasswordMatch) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+        
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '1h'
+        });
+        user.token = token;
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', // Set to true in production
+            maxAge: 3600000 // 1 hour
+        });
+        await user.save();
+        res.status(200).json({ message: 'Login successful', token });
     } catch (error) {
         console.error('Error during login:', error);    
         res.status(500).send('Internal Server Error');
@@ -70,7 +118,11 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-        res.send('Logout endpoint');
+       res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', // Set to true in production
+        });
+        res.status(200).json({ message: 'Logout successful' });
     } catch (error) {
         console.error('Error during logout:', error);   
         res.status(500).send('Internal Server Error');
