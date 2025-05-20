7d03fc2f9644962e899494239f4ae6314aff7331
PrajwalAmbekar <prajwalambekar.231ch039@nitk.edu.in>
2025-05-20 23:11:12 +0530

    fixed the github root

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -1,9 +1,11 @@
 import { LoaderPinwheel } from 'lucide-react';
 import React from 'react'
+import { useThemeStore } from '../stores/useThemeStore.jsx';
 
 const PageLoader = () => {
+  const {theme} = useThemeStore();
   return (
-      <div className='h-screen flex justify-center items-center'>
+      <div className='h-screen flex justify-center items-center' data-theme={theme}>
         <span className='loading loading-spinner loading-lg text-primary'> <LoaderPinwheel/> </span>
       </div>
     )
